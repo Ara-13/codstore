b66c20872b893d9ded60f26887d456913d687ec1
Ara-13 <adavali2020@gmail.com>
2021-10-25 22:03:08 +0330

    4th Part

--- Sell/views.py
+++ Sell/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from . import models
 
-# Create your views here.
+app_name="Sell"
+
+def index(request, ):
+    accounts_list = models.Account.objects.all()
+    context = {
+        'accounts_list': accounts_list,
+    }
+    return render(request, 'Sell/index.html', context)
--- codstore/urls.py
+++ codstore/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('buyaccount/', include('Buy.urls'))
+    path('sellaccount/', include('Sell.urls'))
 ]
