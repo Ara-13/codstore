e63413198aec50962a0276c2d7f21ba104489844
Ara-13 <adavali2020@gmail.com>
2021-10-25 20:24:44 +0330

    Third Part

--- Sell/models.py
+++ Sell/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    code = models.IntegerField()
+    level = models.IntegerField()
+
+    region_account = (
+    ('220' , '220-هند'),
+    ('360' , '360-ایران'),
+    ('560' , '560-اروپا')
+    )
+    region = models.CharField(max_length=3 , choices= region_account)
+
+    rank = models.CharField(max_length=300, blank=True)
+
+    battle_pass = models.CharField(max_length= 400, blank=True)
+
+    description = models.TextField(blank=True)
+
+    price = models.IntegerField()
