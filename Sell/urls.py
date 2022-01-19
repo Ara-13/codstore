ce600c7edf49af21b4d53302cc06e720efeb0dbf
Ara-13 <adavali2020@gmail.com>
2022-01-20 00:55:19 +0330

    Step 1

--- Sell/urls.py
+++ Sell/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     re_path(r'^$', views.index, name='list_view'),
     re_path(r'^(?P<co>[0-9]{1})$', views.account_view, name='detail_view'),
     re_path(r'^0(?P<wco>[0-9]{1})$', views.account_p_view, name='p_detail_view'),
-    re_path(r'^form$', views.account_form)
+    re_path(r'^form$', views.account_form),
+    re_path(r'^login$', views.LoginFunc, name="Login"),
+    re_path(r'^logout$', views.LogoutFunc, name="Logout"),
 ]
--- Sell/views.py
+++ Sell/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from . import models
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from time import sleep
 
 app_name="Sell"
 
@@ -40,6 +44,7 @@ def account_p_view(request, wco):
      }
     return render(request, 'Sell/accounts_info.html', context)
 
+@login_required(login_url='/login')
 def account_form(request, ):
     code = models.Account.objects.all().count()+1
     main_code = models.Account.objects.filter(p_status__exact='w').count()+1
@@ -57,3 +62,31 @@ def account_form(request, ):
         account.save()
 
     return render(request, 'Sell/account_form.html', {'code' : main_code, 'name' : region})
+
+def LoginFunc(request):
+    message = None
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try: 
+            user = User.objects.get(username=username)
+        except:
+            message = "Something is Wrong!"
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            message = "You Successfully logged in!"
+            return redirect('sellaccount:list_view')
+        else:
+            message = "Something is Wrong! Check the Username And Password"
+
+    final_message = message if message != None else ''
+    context = {
+        'message' : final_message,
+    }
+    return render(request, 'Sell/login.html', context)
+
+def LogoutFunc(request):
+    logout(request)
+    return redirect('sellaccount:list_view')
